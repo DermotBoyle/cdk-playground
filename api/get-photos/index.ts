c7f9b56c6381eb9266b966ee88c82c0b48760712
Dermot Boyle <dermotboyled@gmail.com>
2023-09-11 17:27:12 +0200

    added presigned URL access to s3

--- api/get-photos/index.ts
+++ api/get-photos/index.ts
@@ -1,12 +1,46 @@
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2, Context } from 'aws-lambda'
+import { S3 } from 'aws-sdk'
+
+const photoBucketName = process.env.PHOTO_BUCKET_NAME!
+const s3 = new S3()
+
+function generateUrl(asset: S3.Object) {
+	return s3.getSignedUrl('getObject', {
+		Bucket: photoBucketName,
+		Key: asset.Key,
+		Expires: 24 * 60 * 60,
+	})
+}
 
 export const handler = async (event: APIGatewayProxyEventV2, context: Context): Promise<APIGatewayProxyResultV2> => {
-	  return {
-    body: JSON.stringify({ message: 'Hello from Lambda!' }),
-    statusCode: 200,
-    isBase64Encoded: false,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  }
+
+	console.log('event', photoBucketName)
+
+	try {
+		const results = await s3.listObjectsV2({
+			Bucket: photoBucketName,
+		}).promise();
+
+		const allPhotos = await Promise.all(results.Contents?.map(async (asset) => {
+			return {
+				url: generateUrl(asset),
+				key: asset.Key,
+			}
+		}
+		) ?? [])
+
+		return {
+			body: JSON.stringify(allPhotos),
+			statusCode: 200,
+			isBase64Encoded: false,
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		}
+	} catch (err) {
+		return {
+			statusCode: 500,
+			body: JSON.stringify(err)
+		}
+	}
 }
--- lib/cdk-demo-stack.ts
+++ lib/cdk-demo-stack.ts
@@ -2,6 +2,8 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { aws_s3 as s3, aws_lambda_nodejs as lambda } from 'aws-cdk-lib';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
+import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import * as path from 'path';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -15,27 +17,34 @@ export class CdkDemoStack extends cdk.Stack {
       encryption: s3.BucketEncryption.S3_MANAGED,
     });
 
+    const bucketPolicy = new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: [ 's3:ListBucket', 's3:GetObject', 's3:PutObject' ],
+      // Important to have both the bucket ARN and the bucket ARN with /* at the end
+      resources: [ `${bucket.bucketArn}`, `${bucket.bucketArn}/*` ],
+    });
+
+    new BucketDeployment(this, 'CdkDemoBucketDeployment', {
+      sources: [ Source.asset(path.join(__dirname, '..', 'assets')) ],
+      destinationBucket: bucket,
+    });
+
+
     const lambdaFunc = new lambda.NodejsFunction(this, 'CdkDemoLambda', {
       entry: path.join(__dirname, '..', 'api', 'get-photos', 'index.ts'),
       handler: 'handler',
       environment: {
-        BUCKET_NAME: bucket.bucketName,
+        PHOTO_BUCKET_NAME: bucket.bucketName,
       },
       runtime: Runtime.NODEJS_18_X,
     });
+
+    lambdaFunc.addToRolePolicy(bucketPolicy);
 
     new cdk.CfnOutput(this, 'CdkDemoBucketOutput', {
       value: bucket.bucketName,
       description: 'The name of an S3 bucket',
       exportName: 'CdkDemoBucketName',
     });
-
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
